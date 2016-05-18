377603588cbd8edbf554bc889b66f9aec15c6af0
yyang <gdyangyu@gmail.com>
2016-05-18 22:54:57 +0800

    update

--- push_and_deploy.sh
+++ push_and_deploy.sh
@@ -4,4 +4,4 @@ git push
 git push oschina master
 git push yyang master
 gitbook build
-rsync --delete -avz -e ssh _book/* root@www.dddlib.org:/data/gitbook/prof-dev-by-example/
+rsync --delete -avz -e ssh _book/* root@www.dddlib.org:/data/gitbook/books/prof-dev-by-example/
